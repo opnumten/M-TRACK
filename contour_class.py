39cdad402e3ccf1b789ff3b13d931cd5105c771f
opnumten <lancelotzoro@gmail.com>
2019-12-12 16:23:20 -0500

    Update contour_class.py

--- contour_class.py
+++ contour_class.py
@@ -1,10 +1,3 @@
-# Copyright 2007 Zachary Pincus
-# This file is part of CellTool.
-#
-# CellTool is free software; you can redistribute it and/or modify
-# it under the terms of version 2 of the GNU General Public License as
-# published by the Free Software Foundation.
-
 """Classes for dealing with data organized as 2D point clouds and contours.
 
 This module provides a base class, PointSet, for simple operations on sets of
